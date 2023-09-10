e8abe9e0f09ea716752daba4579f55605db24238
azoox-dev <113931638+azoox-dev@users.noreply.github.com>
2023-09-10 20:22:01 +0200

    add form in html

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 game { 'gta5' }
 
 author 'az0ox'
-description 'NUI Admin menu by az0ox'
+description 'NUI inputBans by az0ox'
 version '1.0.0'
-repository 'https://github.com/nathan-echt/azx_admin'
 
 ui_page 'html/index.html'
 
